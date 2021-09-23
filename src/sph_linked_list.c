13942c188a7b98defa472eb0907d3352600320d6
jhelsas <jhelsas@gmail.com>
2021-09-22 23:24:51 -0300

    adding the parsing function to be updated later

--- src/sph_linked_list.c
+++ src/sph_linked_list.c
@@ -138,6 +138,85 @@ int SPHparticleSOA_safe_free(int N,SPHparticle **lsph){
   return 0;
 }
 
+int arg_parse(int argc, char **argv, int64_t *N, double *h,
+              linkedListBox *box){
+  bool intern_h = true;
+
+  box->Xmin = -1.0; box->Ymin = -1.0; box->Zmin = -1.0;
+  box->Xmax =  2.0; box->Ymax =  2.0; box->Zmax =  2.0;
+  
+  if(argc%2==0){
+    printf("wrong number of arguments!\n");
+    printf("Maybe an option is missing a value?\n");
+  }
+
+  for(int i=1;i<argc;i+=2){
+    if( strcmp(argv[i],"-N") == 0 ){
+      *N = (int64_t) atol(argv[i+1]);
+      printf("N particles = %ld\n",*N);
+    }
+    else if( strcmp(argv[i],"-h") == 0 ){
+      *h = atof(argv[i+1]);
+      printf("h = %lf\n",*h);
+      intern_h = false;
+    }
+    else if( strcmp(argv[i],"-Xmin") == 0 ){
+      box->Xmin = atof(argv[i+1]);
+      printf("Xmin = %lf\n",box->Xmin);
+    }
+    else if( strcmp(argv[i],"-Ymin") == 0 ){
+      box->Ymin = atof(argv[i+1]);
+      printf("Ymin = %lf\n",box->Ymin);
+    }
+    else if( strcmp(argv[i],"-Zmin") == 0 ){
+      box->Zmin = atof(argv[i+1]);
+      printf("Zmin = %lf\n",box->Zmin);
+    }
+    else if( strcmp(argv[i],"-Xmax") == 0 ){
+      box->Xmax = atof(argv[i+1]);
+      printf("Xmax = %lf\n",box->Xmax);
+    }
+    else if( strcmp(argv[i],"-Ymax") == 0 ){
+      box->Ymax = atof(argv[i+1]);
+      printf("Ymax = %lf\n",box->Ymax);
+    }
+    else if( strcmp(argv[i],"-Zmax") == 0 ){
+      box->Zmax = atof(argv[i+1]);
+      printf("Zmax = %lf\n",box->Zmax);
+    }
+    else if( strcmp(argv[i],"-Nx") == 0 ){
+      box->Nx   = atol(argv[i+1]);
+      printf("Nx = %ld\n",box->Nx);
+    }
+    else if( strcmp(argv[i],"-Ny") == 0 ){
+      box->Ny   = atol(argv[i+1]);
+      printf("Ny = %ld\n",box->Ny);
+    }
+    else if( strcmp(argv[i],"-Nz") == 0 ){
+      box->Nz   = atol(argv[i+1]);
+      printf("Nz = %ld\n",box->Nz);
+    }
+    else{
+      printf("unknown option: %s %s\n",argv[i],argv[i+1]);
+    }
+  }
+
+  if(intern_h){
+    box->Nx = (int)( (box->Xmax-box->Xmin)/(2*(*h)) );
+    box->Ny = (int)( (box->Ymax-box->Ymin)/(2*(*h)) );
+    box->Nz = (int)( (box->Zmax-box->Zmin)/(2*(*h)) );
+  }
+
+  box->N  = (box->Nx)*(box->Ny)*(box->Nz);
+  
+  double min_val = fmin((box->Xmax-box->Xmin)/box->Nx,fmin((box->Ymax-box->Ymin)/box->Ny,(box->Zmax-box->Zmin)/box->Nz));
+  box->width  = (int)( 0.5 + 2*(*h)/min_val );
+  box->hbegin = kh_init(0);
+  box->hend   = kh_init(1);
+
+  return 0;
+}
+
 int gen_unif_rdn_pos(int64_t N, int seed, SPHparticle *lsph){
 
 	const gsl_rng_type *T=NULL;
