7620479181cf006829ad923506e644c444d6927a
jhelsas <jhelsas@gmail.com>
2021-10-09 02:47:24 -0300

    Setting up statistics to benchmark.

--- quick_run.sh
+++ quick_run.sh
@@ -1,28 +1,28 @@
 cd AoS/
 make clean
 make all
-./bin/example_01-ArrayOfStructs-Naive
-./bin/example_02-ArrayOfStructs-Naive-Omp
-./bin/example_03-ArrayOfStructs-Naive-Omp-SIMD
-./bin/example_04-ArrayOfStructs-Naive-Omp-SIMD-Tiled
-./bin/example_05-ArrayOfStructs-CellLinkedList-serial
-./bin/example_06-ArrayOfStructs-CellLinkedList-innerOmp
-./bin/example_07-ArrayOfStructs-CellLinkedList-innerOmp-SIMD
-./bin/example_08-ArrayOfStructs-CellLinkedList-outerOmp
-./bin/example_09-ArrayOfStructs-CellLinkedList-outerOmp-loadBallanced
+./bin/example_01-ArrayOfStructs-Naive -runs 5
+./bin/example_02-ArrayOfStructs-Naive-Omp -runs 5
+./bin/example_03-ArrayOfStructs-Naive-Omp-SIMD -runs 5
+./bin/example_04-ArrayOfStructs-Naive-Omp-SIMD-Tiled -runs 5
+./bin/example_05-ArrayOfStructs-CellLinkedList-serial -runs 5
+./bin/example_06-ArrayOfStructs-CellLinkedList-innerOmp -runs 5
+./bin/example_07-ArrayOfStructs-CellLinkedList-innerOmp-SIMD -runs 5
+./bin/example_08-ArrayOfStructs-CellLinkedList-outerOmp -runs 5
+./bin/example_09-ArrayOfStructs-CellLinkedList-outerOmp-loadBallanced -runs 5
 cd ..
 cd SoA/
 make clean
 make all
-./bin/example_01-StructOfArrays-Naive
-./bin/example_02-StructOfArrays-Naive-Omp
-./bin/example_03-StructOfArrays-Naive-Omp-SIMD
-./bin/example_04-StructOfArrays-Naive-Omp-SIMD-Tiled
-./bin/example_05-StructOfArrays-CellLinkedList-serial
-./bin/example_06-StructOfArrays-CellLinkedList-InnerOmp
-./bin/example_07-StructOfArrays-CellLinkedList-InnerOmp-SIMD
-./bin/example_08-StructOfArrays-CellLinkedList-OuterOmp
-./bin/example_09-StructOfArrays-CellLinkedList-OuterLoop-LoadBalanced
-./bin/example_10-StructOfArrays-CellLinkedList-OuterLoop-SymmetricalLoadBalancing
+./bin/example_01-StructOfArrays-Naive -runs 5
+./bin/example_02-StructOfArrays-Naive-Omp -runs 5
+./bin/example_03-StructOfArrays-Naive-Omp-SIMD -runs 5
+./bin/example_04-StructOfArrays-Naive-Omp-SIMD-Tiled -runs 5
+./bin/example_05-StructOfArrays-CellLinkedList-serial -runs 5
+./bin/example_06-StructOfArrays-CellLinkedList-InnerOmp -runs 5
+./bin/example_07-StructOfArrays-CellLinkedList-InnerOmp-SIMD -runs 5
+./bin/example_08-StructOfArrays-CellLinkedList-OuterOmp -runs 5
+./bin/example_09-StructOfArrays-CellLinkedList-OuterLoop-LoadBalanced -runs 5
+./bin/example_10-StructOfArrays-CellLinkedList-OuterLoop-SymmetricalLoadBalancing -runs 5
 cd .. 
 python diff_ref.py 
